524d07b1b69916ec8742fdda21d276a028005da8
sandeepbikkana <sandeepbikkana@gmail.com>
2025-08-20 12:53:56 +0530

    run_ansible.sh

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,9 +1,3 @@
-cat > ~/self-heal/run_ansible.sh << 'SH'
-#!/usr/bin/env bash
-set -e
-ALERTNAME="$1"
-LOG="/var/log/selfheal-runner.log"
-echo "$(date -Is) Running Ansible for alert: ${ALERTNAME}" | tee -a "$LOG"
-ansible-playbook /home/ubuntu/self-heal/heal.yml --extra-vars "alertname=${ALERTNAME}" | tee -a "$LOG"
-SH
-chmod +x ~/self-heal/run_ansible.sh
+#!/bin/bash
+ansible-playbook /home/ubuntu/self-heal/heal.yml >> /home/ubuntu/selfheal-ansible.log 2>&1
+
